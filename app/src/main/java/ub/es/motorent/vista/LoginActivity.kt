4786267bac0d7ac92de07df7dd5bbf993d759f81
juditRamo <jramosol8@alumnes.ub.edu>
2020-10-25 13:08:42 +0100

    activities created

---
+++ app/src/main/java/ub/es/motorent/vista/LoginActivity.kt
@@ -0,0 +1,13 @@
+package ub.es.motorent.vista
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ub.es.motorent.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
