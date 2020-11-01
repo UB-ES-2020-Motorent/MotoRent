fdc05eb2c22660edbc894baa1129ea76c06ad023
RicardPlanes <31952485+RicardPlanes@users.noreply.github.com>
2020-11-01 20:42:42 +0100

    Google Maps API

--- app/src/main/java/ub/es/motorent/app/view/MapsActivity.kt
+++ app/src/main/java/ub/es/motorent/app/view/MapsActivity.kt
@@ -37,8 +37,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val sydney = LatLng(41.3818, 2.1685)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Barcelona"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15.0f))
     }
 }
