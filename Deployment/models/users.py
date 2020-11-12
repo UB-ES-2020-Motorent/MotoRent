5db47e5f52f8bf1f8d237d2bff5af0b0a7d49c99
arnau <greadercf9@gmail.com>
2020-11-12 19:40:19 +0100

    testing api deploy

--- Deployment/models/users.py
+++ Deployment/models/users.py
@@ -7,19 +7,29 @@ class UsersModel(db.Model):
     """
 
     __tablename__ = 'users'
-    id = db.Column(db.Integer(), primary_key=True, unique=True, nullable=False,autoincrement=True)
-    name = db.Column(db.String(), nullable=False)
+    # id = db.Column(db.Integer(), ForeignKey('renting.id'), primary_key=True, unique=True, nullable=False,
+    # autoincrement=True)
+    # id_bank_data = db.Column(db.Integer(), ForeignKey('bank_data.user_id'), ForeignKey('payments.user_id'),
+    # nullable=False, unique=True, autoincrement=True)
+    id = db.Column(db.Integer(), primary_key=True, unique=True, nullable=False, autoincrement=True)
+    id_bank_data = db.Column(db.Integer(), nullable=True, unique=True)
+    national_id_document = db.Column(db.String(), nullable=True)
+    country = db.Column(db.String(), nullable=True)
+    name = db.Column(db.String(), nullable=True)
     surname = db.Column(db.String(), nullable=True)
-    mail = db.Column(db.String(), nullable=False)
-    google_token = db.Column(db.String(), nullable=False)
+    mail = db.Column(db.String(), nullable=False, unique=True)
+    google_token = db.Column(db.String(), nullable=False, unique=True)
     role = db.Column(db.Integer(), nullable=False)
 
-    def __init__(self, name, surname, mail, google_token, role=0):
-        self.name = name
-        self.surname = surname
+    def __init__(self, mail, google_token, role):
+        self.national_id_document = None
+        self.country = None
+        self.name = None
+        self.surname = None
         self.mail = mail
         self.google_token = google_token
         self.role = role
+        self.id_bank_data = None
 
     def json(self, role):
         """
@@ -29,6 +39,9 @@ class UsersModel(db.Model):
         if role == 1:
             return {
                 'id': self.id,
+                'id_bank_data': self.id_bank_data,
+                'national_id_document': self.national_id_document,
+                'country': self.country,
                 'name': self.name,
                 'surname': self.surname,
                 'mail': self.mail,
@@ -38,7 +51,8 @@ class UsersModel(db.Model):
         else:
             return {
                 'name': self.name,
-                'surname': self.surname
+                'surname': self.surname,
+                'country': self.country
             }
 
     def save_to_db(self):
@@ -56,10 +70,10 @@ class UsersModel(db.Model):
         db.session.commit()
 
     @classmethod
-    def find_by_username(cls, name):
+    def find_by_name(cls, name):
         """
-        Finds an user by username
-        Param: number id
+        Finds an user by name
+        Param: number name
         Return: UsertModel
         """
         return UsersModel.query.filter_by(name=name).first()
@@ -72,6 +86,24 @@ class UsersModel(db.Model):
         Return: ArtistModel
         """
         return UsersModel.query.filter_by(id=user_id).first()
+
+    @classmethod
+    def find_by_mail(cls, mail):
+        """
+        Finds an Artist by ID
+        Param: number id
+        Return: ArtistModel
+        """
+        return UsersModel.query.filter_by(mail=mail).first()
+
+    @classmethod
+    def find_by_id_bank_data(cls, id_bank_data):
+        """
+        Finds an Artist by ID
+        Param: number id
+        Return: ArtistModel
+        """
+        return UsersModel.query.filter_by(id_bank_data=id_bank_data).first()
 
     @classmethod
     def all_users(cls):
