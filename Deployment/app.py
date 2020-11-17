e186a294bd211e775c3266c5a86490b0c2aede33
JoanMartinMartrus <joan71995@gmail.com>
2020-11-17 21:43:45 +0100

    Fix

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,28 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 
-from db import db, init_db
-from decouple import config as config_decouple
-from config import config
-
 from resources.users import Users, UsersList
 from resources.motos import Motos, MotosList
 from resources.map_coords import MapCoords, MapCoordsList
+from resources.rentals import Rentals, RentalsList
+from resources.bank_data import BankData, BankDataList
 
+from db import db, secret_key
 
 app = Flask(__name__)
-environment = config['development']
-if config_decouple('PRODUCTION', cast=bool, default=False):
-    environment = config['production']
-app.config.from_object(environment)
-
-init_db(app)
-
+app.config.from_object(__name__)
 api = Api(app)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secret_key
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -41,6 +38,12 @@ api.add_resource(Motos, '/moto', '/moto/<int:id>', '/moto/<string:license_number
 
 api.add_resource(MapCoords, '/mapcoord')
 api.add_resource(MapCoordsList, '/mapcoords')
+
+api.add_resource(Rentals, '/rental', '/rental/<int:id>')
+api.add_resource(RentalsList, '/rentals')
+
+api.add_resource(BankData, '/bankdata', '/bankdata/<string:id_bank_data>')
+api.add_resource(BankDataList, '/bankdatas')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
