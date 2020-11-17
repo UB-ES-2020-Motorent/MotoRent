cb91440c54de491380792263d4d61e79fae505ba
arnau <greadercf9@gmail.com>
2020-11-17 18:59:14 +0100

    ubicacio usuari, logos nous

--- AndroidApp/app/src/main/java/ub/es/motorent/app/view/MapsActivity.kt
+++ AndroidApp/app/src/main/java/ub/es/motorent/app/view/MapsActivity.kt
@@ -1,20 +1,42 @@
 package ub.es.motorent.app.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.*
 import ub.es.motorent.R
 
-class MapsActivity : FullScreenActivity(), OnMapReadyCallback {
+
+class MapsActivity : FullScreenActivity(), LocationListener, OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    lateinit var locationManager: LocationManager
+    lateinit var coordenadas: LatLng
+    lateinit var marker_user : Marker
+    lateinit var marker1: Marker
+    lateinit var marker2: Marker
+    lateinit var marker3: Marker
+    lateinit var marker4: Marker
+    lateinit var marker5: Marker
+    lateinit var marker6: Marker
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +45,38 @@ class MapsActivity : FullScreenActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {}
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1.toFloat(), this)
     }
+
     override fun onBackPressed() {
         //DO NOTHING
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        coordenadas = LatLng(location?.latitude as Double, location?.longitude)
+        marker_user.position = coordenadas
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("Not yet implemented")
     }
 
     /**
@@ -40,19 +91,35 @@ class MapsActivity : FullScreenActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val coordenadas = LatLng(41.3818, 2.1685)
+
+        var currentLocation = if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            val permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions,0)
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+
+        coordenadas = LatLng(currentLocation?.latitude as Double, currentLocation?.longitude)
+
         val coordMoto1 = LatLng(41.3818, 2.1685)
         val coordMoto2 = LatLng(41.382093, 2.131414)
         val coordMoto3 = LatLng(41.402959, 2.174802)
         val coordMoto4 = LatLng(41.413352, 2.202810)
         val coordMoto5 = LatLng(41.437218, 2.180026)
         val coordMoto6 = LatLng(41.411589, 2.152448)
-        mMap.addMarker(MarkerOptions().position(coordMoto1).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
-        mMap.addMarker(MarkerOptions().position(coordMoto2).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
-        mMap.addMarker(MarkerOptions().position(coordMoto3).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
-        mMap.addMarker(MarkerOptions().position(coordMoto4).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
-        mMap.addMarker(MarkerOptions().position(coordMoto5).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
-        mMap.addMarker(MarkerOptions().position(coordMoto6).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+
+        marker_user = mMap.addMarker(MarkerOptions().position(coordenadas).icon(BitmapDescriptorFactory.fromResource(R.drawable.you_are_here_resized)))
+
+        marker1 = mMap.addMarker(MarkerOptions().position(coordMoto1).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+        marker2 = mMap.addMarker(MarkerOptions().position(coordMoto2).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+        marker3 = mMap.addMarker(MarkerOptions().position(coordMoto3).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+        marker4 = mMap.addMarker(MarkerOptions().position(coordMoto4).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+        marker5 = mMap.addMarker(MarkerOptions().position(coordMoto5).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+        marker6 = mMap.addMarker(MarkerOptions().position(coordMoto6).icon(BitmapDescriptorFactory.fromResource(R.drawable.motoicon)))
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenadas, 17.0f))
         val hole = listOf(
             LatLng(41.346835, 2.139348),
@@ -83,19 +150,21 @@ class MapsActivity : FullScreenActivity(), OnMapReadyCallback {
             LatLng(41.374874, 2.188226),
             LatLng(41.381653, 2.184492),
             LatLng(41.352369, 2.162530),
-            LatLng(41.346546, 2.139268))
+            LatLng(41.346546, 2.139268)
+        )
 
         val hollowPolygon = mMap.addPolygon(
             PolygonOptions().add(
-            LatLng(58.950017, -16.157126),
-            LatLng(58.950017, 26.123910),
-            LatLng(25.943059, 26.123910),
-            LatLng(25.943059, -16.157126)
-        ).addHole(hole)
-            .fillColor(Color.GRAY)
-            .strokeWidth(5.0f)
-            .fillColor(0x55686868)
-            .geodesic(true)
+                LatLng(58.950017, -16.157126),
+                LatLng(58.950017, 26.123910),
+                LatLng(25.943059, 26.123910),
+                LatLng(25.943059, -16.157126)
+            ).addHole(hole)
+                .fillColor(Color.GRAY)
+                .strokeWidth(5.0f)
+                .fillColor(0x55686868)
+                .geodesic(true)
         )
     }
 }
+
--- AndroidApp/app/src/main/java/ub/es/motorent/app/view/WelcomeActivity.kt
+++ AndroidApp/app/src/main/java/ub/es/motorent/app/view/WelcomeActivity.kt
@@ -1,13 +1,21 @@
 package ub.es.motorent.app.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import ub.es.motorent.R
 import ub.es.motorent.app.model.UserDB
+import ub.es.motorent.app.model.UserJson
 import ub.es.motorent.app.presenter.WelcomePresenter
 import java.util.*
 import kotlin.concurrent.schedule
@@ -15,6 +23,7 @@ import kotlin.concurrent.schedule
 class WelcomeActivity : FullScreenActivity() {
 
     private lateinit var presenter: WelcomePresenter
+    private val REQUEST_PERMISSION_FINE_LOCATION = 1
 
     var timer = Timer()
 
@@ -24,16 +33,53 @@ class WelcomeActivity : FullScreenActivity() {
 
         presenter = WelcomePresenter(this)
 
-        timer.schedule(3000) {
-            startActivity(presenter.navigationPath())
-            finish()
-        }
+        showPhoneStatePermission()
 
     }
 
     override fun onPause() {
         timer.cancel()
         super.onPause()
+    }
+
+    private fun showPhoneStatePermission() {
+        val permissionCheck = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_FINE_LOCATION);
+            } else {
+                requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSION_FINE_LOCATION)
+            }
+        } else {
+            timer.schedule(3000) {
+                startActivity(presenter.navigationPath())
+                finish()
+            }
+        }
+    }
+
+    private fun showExplanation(title: String, message: String, permission: String, permissionRequestCode: Int) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title).setMessage(message).setPositiveButton(android.R.string.ok,DialogInterface.OnClickListener {
+                dialog, id -> requestPermission(permission, permissionRequestCode)
+                })
+        builder.create().show()
+    }
+
+    private fun requestPermission(permissionName: String, permissionRequestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permissionName), permissionRequestCode)
+    }
+
+    @Override
+    override fun onRequestPermissionsResult(requestCode : Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1) {
+            if (!grantResults.contains(PackageManager.PERMISSION_DENIED)) {
+                startActivity(presenter.navigationPath())
+                finish()
+            }
+        }
     }
 
 }
