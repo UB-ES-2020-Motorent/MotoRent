8c9c77feb300d58a68aaf155461ca0904b2beeb2
arnau <greadercf9@gmail.com>
2020-11-22 20:38:37 +0100

    deploy

--- Deployment/app.py
+++ Deployment/app.py
@@ -31,7 +31,7 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():
-    return 'MotoRent Database.'
+    return 'MotoRent Database'
 
 
 api.add_resource(Users, '/user/<string:user_id>', '/user')
