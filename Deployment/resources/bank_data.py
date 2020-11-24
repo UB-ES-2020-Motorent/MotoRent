fc408fb83461e9a303d8aaed5ebf2f4d7fce9aa2
arnau <greadercf9@gmail.com>
2020-11-24 02:50:23 +0100

    les 3 issues acabades, swagger i frontend ja actualitzats en conseqüencia

--- Deployment/resources/bank_data.py
+++ Deployment/resources/bank_data.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.bank_data import BankDataModel
+from models.users import UsersModel
 
 parser = reqparse.RequestParser()
 parser.add_argument('id_bank_data', type=str, required=False, help="Account bank data id, This field cannot be left "
@@ -10,6 +11,7 @@ parser.add_argument('card_owner', type=str, required=False, help="Card owner, Th
 parser.add_argument('card_cvv', type=str, required=False, help="Card CVV, This field cannot be left blank")
 parser.add_argument('card_expiration', type=str, required=False, help="Card expiration date,"
                                                                       " This field cannot be left blank")
+parser.add_argument('view_all', type=str, required=False, help="True for all cards, False for only default card")
 
 
 class BankData(Resource):
@@ -29,17 +31,20 @@ class BankData(Resource):
 
         arguments = sum([1 if value else 0 for value in data.values()])
 
-        if arguments > 1:
+        if (arguments > 2 and data['view_all']) or (arguments > 1 and not data['view_all']):
             return {'message': "Please filter by only one feature"}, 400
-        if arguments <= 0:
+        if (arguments <= 1 and data['view_all']) or (arguments <= 0 and not data['view_all']):
             return {'message': "Please filter by at least one feature: user id, bank data id or card number"}, 400
 
         if data['user_id']:
-            bank_datas = BankDataModel.find_by_user_id(user_id=data['user_id'])
+            if str(data['view_all']).lower() == 'true':
+                bank_datas = BankDataModel.find_by_user_id(user_id=data['user_id'], view_all=True)
+            else:
+                bank_datas = BankDataModel.find_by_user_id(user_id=data['user_id'], view_all=False)
             if bank_datas:
                 return {'BankData': [bank_data.json() for bank_data in bank_datas]}, 200
             else:
-                return {'message': "Data with {} [{}] Not found".format('id', data['user_id'])}, 404
+                return {'message': "Data for {} [{}] Not found".format('user with id', data['user_id'])}, 404
         elif data['id_bank_data']:
             bank_data = BankDataModel.find_by_id_bank_data(id_bank_data=data['id_bank_data'])
             if bank_data:
@@ -93,14 +98,24 @@ class BankData(Resource):
             return {'message': "Data with user id [{}] and card number [{}] already exists".format(
                 data['user_id'], data['card_number'])}, 409
 
-        user = BankDataModel(user_id=data['user_id'], card_number=data['card_number'], card_owner=data['card_owner'],
-                             card_cvv=data['card_cvv'], card_expiration=data['card_expiration'])
+        user = UsersModel.find_by_id(data['user_id'])
 
-        try:
-            user.save_to_db()
-            return {'BankData': BankDataModel.find_by_user_id_and_card_number(user.user_id, user.card_number).json()}, 201
-        except:
-            return {"message": "Error Description"}, 500
+        if user:
+            bdata = BankDataModel(user_id=data['user_id'], card_number=data['card_number'], card_owner=data['card_owner'],
+                                 card_cvv=data['card_cvv'], card_expiration=data['card_expiration'])
+            try:
+                bdata.save_to_db()
+                try:
+                    if not user.id_bank_data:
+                        user.id_bank_data = bdata.id_bank_data
+                    user.save_to_db()
+                except:
+                    return {'BankData': "Bank data was created, but could not update user card preferences"}, 201
+                return {'message': BankDataModel.find_by_user_id_and_card_number(bdata.user_id, bdata.card_number).json()}, 201
+            except:
+                return {"message": "Error Description"}, 500
+        else:
+            return {'message': "User with id [{}] does not exist".format(data['user_id'])}, 409
 
     def put(self, id_bank_data):
         """
@@ -111,28 +126,28 @@ class BankData(Resource):
         """
         data = parser.parse_args()
 
-        user = BankDataModel.find_by_id_bank_data(id_bank_data=id_bank_data)
+        bdata = BankDataModel.find_by_id_bank_data(id_bank_data=id_bank_data)
 
         if data['id_bank_data']:
-            user.id_bank_data = data["id_bank_data"]
+            bdata.id_bank_data = data["id_bank_data"]
 
         if data['user_id']:
-            user.user_id = data["user_id"]
+            bdata.user_id = data["user_id"]
 
         if data['card_number']:
-            user.card_number = data["card_number"]
+            bdata.card_number = data["card_number"]
 
         if data['card_owner']:
-            user.card_owner = data["card_owner"]
+            bdata.card_owner = data["card_owner"]
 
         if data['card_cvv']:
-            user.card_cvv = data["card_cvv"]
+            bdata.card_cvv = data["card_cvv"]
 
         if data['card_expiration']:
-            user.card_expiration = data["card_expiration"]
+            bdata.card_expiration = data["card_expiration"]
 
         try:
-            user.save_to_db()
+            bdata.save_to_db()
             return {'BankData': BankDataModel.find_by_id_bank_data(id_bank_data).json()}, 200
         except:
             return {"message": "Error Description"}, 500
@@ -145,11 +160,11 @@ class BankData(Resource):
         Return: dict (message ok / message)
         """
 
-        user = BankDataModel.find_by_id_bank_data(id_bank_data=id_bank_data)
+        bdata = BankDataModel.find_by_id_bank_data(id_bank_data=id_bank_data)
 
-        if user:
+        if bdata:
             try:
-                user.delete_from_db()
+                bdata.delete_from_db()
                 return {'message': "Data with bank data id [{}] and all associated info deleted".format(id_bank_data)
                         }, 200
             except:
