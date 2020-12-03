924249af5d64cfa3c63e053471ed2ee0e932d051
juditRamo <jramosol8@alumnes.ub.edu>
2020-12-03 23:54:34 +0100

    FrontWeb created

---
+++ FrontWeb/src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1> {{ message }} </h1>
+    <button class="btn btn-success btn-lg" @click="addLike"> like </button>
+    <h4> Total tickets bought: {{ likes }} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      message: 'Ramo Industries',
+      likes: 0
+    }
+  },
+  methods: {
+    addLike () {
+      this.likes += 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ FrontWeb/src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    }
+  ]
+})
