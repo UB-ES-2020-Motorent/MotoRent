fb505b0e190506ff44e99bf8d4d4346cea08ccc4
RicardPlanes <31952485+RicardPlanes@users.noreply.github.com>
2020-12-05 19:23:26 +0100

    Llista d'usuaris a la web + botons per canviar de llista usuaris / motos

---
+++ FrontWeb/src/components/Users.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1>Usuaris</h1>
+    <div class="container">
+      <b-table striped hover :items="users"></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'User',
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    getUsers () {
+      const path = `https://motorent-apitest.herokuapp.com/users?admin_code=admin_secret_code`
+      axios.get(path)
+        .then((res) => {
+          this.users = res.data.users
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    deleteUser (user) {
+
+    }
+  },
+  created () {
+    this.getUsers()
+  }
+}
+</script>
