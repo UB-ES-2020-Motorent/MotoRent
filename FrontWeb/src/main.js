30a2e4771d9ac5eed7603172cb3dba0cbd2e367c
juditRamo <jramosol8@alumnes.ub.edu>
2020-12-07 10:54:48 +0100

    #155 Plantilla Llista i Afegit i Eliminar (Motos)

--- FrontWeb/src/main.js
+++ FrontWeb/src/main.js
@@ -1,8 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import BootstrapVue from 'bootstrap-vue'
 import '@/../bootstrap/css/bootstrap.css'
-// import 'bootstrap/dist/css/bootstrap-vue.min.css'
+import BootstrapVue from 'bootstrap-vue'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
